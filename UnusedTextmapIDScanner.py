8e3ed5696a4806c497e9f313355ec3064b5b0f82
zisi.mo <zisi.mo@mihoyo.com>
2018-09-08 21:36:16 +0800

    Demo UnusedTextmapIDScanner.py

--- UnusedTextmapIDScanner.py
+++ UnusedTextmapIDScanner.py
@@ -42,25 +42,45 @@ class UnusedTextmapIDScanner:
         allIDs = None
         with open(allIDsFilePath, "r", encoding="utf-8") as allIDsFile:
             allIDs = [i.strip() for i in allIDsFile]
-        allTargetFiles = [targetDir + "\\" + i for i in os.listdir(targetDir) if (not re.match("TextMap", i) and not re.match("Textmap", i)) and (os.path.splitext(i)[1]==".txt")]
-        fileCount = len(allTargetFiles)
+        allTargetFilesPath = [targetDir + "\\" + i for i in os.listdir(targetDir) if (not re.match("TextMap", i) and not re.match("Textmap", i)) and os.path.splitext(i)[1]==".txt"]
+        #allTargetFilesPath = self._getAllCS(targetDir)
+        #allIDs = allIDs[27006:27047]
+        #allIDs = allIDs[3274:3315]
+        #allIDs = allIDs[3371:3387]
+        fileCount = len(allTargetFilesPath)
         fileidx = 1
-        for targetFilePath in allTargetFiles:
-            with open(targetFilePath, "r", encoding="utf-8") as targetFile:
-                sys.stdout.write("processing %d/%d\r" % (fileidx, fileCount))
-                sys.stdout.flush()
-                fileidx += 1
-                removeIDs = []
-                for id in allIDs:
-                    for line in targetFile:
-                        if id in line:
-                            removeIDs.append(id)
-                            targetFile.seek(0)
-                            break
-                #print(removeIDs)
-                for id in removeIDs:
-                    allIDs.remove(id)
-        
+        allFiles = []
+        for targetFilePath in allTargetFilesPath:
+            allFiles.append(open(targetFilePath, "r", encoding="utf-8"))
+            sys.stdout.write("loading %d/%d\r" % (fileidx, fileCount))
+            sys.stdout.flush()
+            fileidx += 1
+        sys.stdout.write("\nloading Done!\n")
+        sys.stdout.flush()
+
+        print(len(allIDs))
+        fileidx = 0
+        for targetFile in allFiles:
+            fileidx += 1
+            removeIDs = []
+            for id in allIDs:
+                p = "[\s]"+id+"[\s]"
+                for line in targetFile:
+                    #if line.find(id) != -1:
+                    if re.search(p, line):
+                        removeIDs.append(id)
+                        break
+            if (len(removeIDs) > 0):
+                print("processing %d/%d %s" %(fileidx, fileCount, targetFile.name))
+                print(removeIDs)
+            for id in removeIDs:
+                allIDs.remove(id)
+            if (len(removeIDs) > 0):
+                print(len(allIDs))
+
+        for targetFile in allFiles:
+            targetFile.close()
+
         with open(self._getUnusedIDsOutputFileName(outputDir), "x", encoding="utf-8") as outputFile:
             for id in allIDs:
                 outputFile.write(id + "\n")
@@ -68,13 +88,17 @@ class UnusedTextmapIDScanner:
         sys.stdout.flush()
         pass
 
+    def _getAllCS(self, csDir):
+        result = []
+        for root, dirs, files in os.walk(csDir):
+            for f in files:
+                if os.path.splitext(f)[1]==".cs":
+                    result.append(os.path.join(root, f))
+        return result
+
 if __name__ == "__main__":
+    os.system("cls")
     s = UnusedTextmapIDScanner()
     #s.ScanAllTextmap(r"E:\ng_hsod_master\Assets\Resources\Data\_ExcelOutput", r"E:\\")
     s.FindAllUnusedIDs(r"E:\AllTextmapIDs-20180908-193508.txt", r"E:\ng_hsod_master\Assets\Resources\Data\_ExcelOutput", r"E:\\")
-    #x = [r"C:\Work" +"\\" + i for i in os.listdir(r"C:\Work") if re.match("Textmap", i)]
-    #print(x)
-    #with open(r"C:\Work\AllTextmapIDs-20180908-170935.txt", "r") as f:
-    #    x = [i.strip() for i in f]
-    #    print(x)
-    
+    #s.FindAllUnusedIDs(r"E:\AllTextmapIDs-20180908-193508.txt", r"E:\ng_hsod_master\Assets\MoleMole", r"E:\\")
