5d69fa856270d03877aad091f693bedcc70b2311
zisi.mo <zisi.mo@mihoyo.com>
2018-09-12 11:14:33 +0800

    TextmapSpliter.py v0.1

--- TextmapSpliter.py
+++ TextmapSpliter.py
@@ -1,22 +1,68 @@
 import os
 import openpyxl
+import datetime
+import sys
 
 class Spliter(object):
-    def Run(self, targetFile, maxRow):
+    def Run(self, targetFile, maxRow = 0):
         rowOneCellNames = ["A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1", "I1", "J1"]
         book = openpyxl.load_workbook(targetFile, read_only=True)
         sheet = book[book.sheetnames[0]]
-        sheet.max_row = maxRow
+        if maxRow != 0:
+            sheet.max_row = maxRow
         columsName = []
         for i in rowOneCellNames:
             columsName.append(sheet[i].value)
-        print(columsName)
+        
+        rowIndex = 1
+        maxIndex = sheet.max_row
+        allData = {}
+        isFirstRow = True
+        for row in sheet:
+
+            sys.stdout.write("processing index %d/%d \r" %(rowIndex, maxIndex))
+            sys.stdout.flush()
+            rowIndex += 1
+
+            if isFirstRow:
+                isFirstRow = False
+                continue
+            rowData = None
+            rowID = ""
+            oneRow = []
+            for cell in row:
+                if rowData == None:
+                    rowID = cell.value
+                    rowData = allData.get(rowID)
+                    if rowData == None:
+                        rowData = []
+                        allData[rowID] = rowData
+                oneRow.append(cell.value)
+            rowData.append(oneRow)
+        print("\n process Done!")
+        
+        print("Begin write files")
+        fileIndex = 1
+        maxFile = len(allData)
+        for fileName in allData:
+            if (fileName == None)
+                continue
+            sys.stdout.write("processing file %d / %d \r" %(fileIndex, maxFile))
+            sys.stdout.flush()
+            fileIndex += 1
+
+            writeBook = openpyxl.Workbook(write_only=True)
+            writeSheet = writeBook.create_sheet()
+            writeSheet.append(columsName)
+            for row in allData[fileName]:
+                writeSheet.append(row)
+            writeBook.save(fileName+".xlsx")
+        print("\n Write Done!")
 
 if __name__ == "__main__":
-    print("Begin ...")
+    print("Begin ... at %s " % datetime.datetime.now())
     filename = "TextMapMerge去重-2.3 only-en.xlsx"
-    maxRow = 38613
-    #filename = "e1.xlsx"
+    maxRow = 38615
     s = Spliter()
     s.Run(filename, maxRow)
-    print("Done!")
+    print("Done! at %s " % datetime.datetime.now())
